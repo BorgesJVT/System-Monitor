a1f4392c598777e89a1b6dd2100c110f100cb73c
BorgesJVT <joao.victor.tb@gmail.com>
2023-01-30 23:07:02 -0300

    implementing format file

--- src/format.cpp
+++ src/format.cpp
@@ -1,4 +1,6 @@
 #include <string>
+#include <iomanip>
+#include <sstream>
 
 #include "format.h"
 
@@ -8,4 +10,16 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+  long minutes = seconds / 60.0;
+  int hours = minutes / 60.0;
+  minutes = minutes % 60;
+  int remaining_seconds = seconds % 60;
+
+  // std::ostream& os;
+  std::ostringstream oss;
+  oss << std::setfill('0') << std::setw(2) << std::to_string(hours) 
+     << ':' << std::setfill('0') << std::setw(2) << std::to_string(minutes)
+     << ':' << std::setfill('0') << std::setw(2) << std::to_string(remaining_seconds) << '.';
+  return oss.str(); 
+}
